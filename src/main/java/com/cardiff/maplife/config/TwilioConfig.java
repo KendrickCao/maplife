76ad76bc2c0855a649e00c91fac92588645a57c3
Yanguo Cao <478218222@qq.com>
2023-01-09 23:51:53 +0000

    change twolio credentials

--- src/main/java/com/cardiff/maplife/config/TwilioConfig.java
+++ src/main/java/com/cardiff/maplife/config/TwilioConfig.java
@@ -10,10 +10,12 @@ import java.util.Arrays;
 @ConfigurationProperties("twilio")
 public class TwilioConfig {
     private static final String account_SID = "AC9ae823ff5ee8a94680970b9fa9a72525";
-    private static final String api_key = "SK2d1ac24d8326384f3852177c71c5db2e";
-    private static final String api_secret = "h2GJuQJiolGdH9R7rh9oOTWAhrp36mf3";
-    private static final String token ="d04dd2cc2e5ae13e297ba365cda9f8ab";
-    private static final String chat_service_SID = "IS78808bd2d6294dc6a60c4c504f20e42b";
+    private static final String api_key = "SKd5e27d18f68df09e8b74e1a09d764748";
+    private static final String api_secret = "isvulCw9DtV3pTFEOz8DAJfvef8aB7l9";
+    // private static final String api_key = "SK2d1ac24d8326384f3852177c71c5db2e";
+    // private static final String api_secret = "h2GJuQJiolGdH9R7rh9oOTWAhrp36mf3";
+    private static final String token ="1f34f77af2e7366e74f522038b80b39d";
+    private static final String chat_service_SID = "IS48b03cf21f4241abb3b80e01884e86da";
     public TwilioConfig() {
     }
 

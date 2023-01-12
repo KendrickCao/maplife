e4d49066d33ff9ff20936c947a23e4919ce6e784
Yanguo Cao <478218222@qq.com>
2023-01-12 19:48:35 +0000

    fix bug of the marker

--- src/main/java/com/cardiff/maplife/config/WebMvcConfig.java
+++ src/main/java/com/cardiff/maplife/config/WebMvcConfig.java
@@ -1,5 +1,8 @@
 package com.cardiff.maplife.config;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -13,8 +16,20 @@ public class WebMvcConfig implements WebMvcConfigurer {
         exposeDirectory("image", registry);
     }
     private void exposeDirectory(String dirName, ResourceHandlerRegistry registry) {
-    //if linux
+        String osName=System.getProperty("os.name");
+        //if windows
+        if (osName.toLowerCase().startsWith("win")){
+            Path uploadDir2 = Paths.get(dirName);
+            String uploadPath = uploadDir2.toFile().getAbsolutePath();
+            String realUploadPath="\\src\\main\\resources\\static\\image";
+            uploadPath = uploadPath.replace("image", realUploadPath);
             registry.addResourceHandler("/image/**")
-            .addResourceLocations("file:/maplife/image/");
-    }
+                    .addResourceLocations("file:/"+ uploadPath + "/");
+                    System.out.println("windows");
+        }else{
+        //if linux
+                registry.addResourceHandler("/image/**")
+                .addResourceLocations("file:/maplife/image/");
+            }
+        }
 }
--- src/main/java/com/cardiff/maplife/fileUpload/EventMVConfig.java
+++ src/main/java/com/cardiff/maplife/fileUpload/EventMVConfig.java
@@ -1,5 +1,8 @@
 package com.cardiff.maplife.fileUpload;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -11,9 +14,19 @@ public class EventMVConfig implements WebMvcConfigurer {
         exposeDirectory("event", registry);
     }
     private void exposeDirectory(String dirName, ResourceHandlerRegistry registry) {
-    //if linux
+        Path uploadDir = Paths.get(dirName);
+        String uploadPath = uploadDir.toFile().getAbsolutePath();
+        String osName=System.getProperty("os.name");
+        //if windows
+        if (osName.toLowerCase().startsWith("win")){
+            if (dirName.startsWith("../")) dirName = dirName.replace("../", "");
+            registry.addResourceHandler("/" + dirName + "/**").addResourceLocations("file:/"+ uploadPath + "/");
+        }
+        else{
+            //if linux
             registry.addResourceHandler("/event/**")
             .addResourceLocations("file:/maplife/event/");
+        }
     }
 
 }
